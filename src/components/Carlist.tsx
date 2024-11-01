607682fa9116d0cf401a42669e47532165dbb2c7
Invalid <zappeln@gmail.com>
2024-10-31 22:15:50 -0500

    delete car and Snackbar

--- src/components/Carlist.tsx
+++ src/components/Carlist.tsx
@@ -1,9 +1,14 @@
-import { useQuery } from '@tanstack/react-query'
-import { DataGrid, GridColDef, GridCellParams } from '@mui/x-data-grid';
-import { getCars } from '../api/carapi'
+import { useState } from 'react'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { DataGrid, GridColDef, GridCellParams } from '@mui/x-data-grid'
+import Snackbar from '@mui/material/Snackbar'
+import { getCars, deleteCar } from '../api/carapi'
 
 
 function Carlist() {
+  const [open, setOpen] = useState(false);
+  const queryClient = useQueryClient();
+
   const { data, error, isLoading, isSuccess } = useQuery({
     queryKey: ["cars"],
     queryFn: getCars
@@ -16,17 +21,28 @@ function Carlist() {
     {field:'registrationNumber', headerName: 'Reg.no', width: 150},
     {field:'modelYear', headerName: 'Model Year', width: 150},
     {field:'price', headerName: 'Price', width: 150},
-    {field: 'delete', headerName: '', width: 90, sortable: false, filterable: false, disableColumnMenu: true,
+    {field: 'delete', headerName: '', width: 90, sortable: false, 
+      filterable: false, disableColumnMenu: true,
       renderCell: (params: GridCellParams) => (
-        <button onClick={() => alert(params.row._links.carhref)}>
+        <button onClick={() => mutate(params.row._links.car.href)}>
           Delete
         </button>
       )
     }
   ]
+
+  const { mutate } = useMutation(deleteCar, {
+    onSuccess: () => {
+      setOpen(true);
+      queryClient.invalidateQueries({ queryKey: ['cars']});
+    },
+    onError: (err) => {
+      console.log(err);
+    }
+  })
   
   if (isLoading) {
-    return <span>Loading...{isLoading}</span>
+    return <span>Loading... </span>
   }
   else 
   if (error) {
@@ -34,11 +50,20 @@ function Carlist() {
   }
   else if (isSuccess) {
     return (
-      <DataGrid
-        rows={data}
-        columns={columns}
-        getRowId={row => row._links.self.href}
-      />
+      <>
+        <DataGrid
+          rows={data}
+          columns={columns}
+          disableRowSelectionOnClick={true}
+          getRowId={row => row._links.self.href}
+        />
+        <Snackbar
+          open={open}
+          autoHideDuration={2000}
+          onClose={() => setOpen(false)}
+          message="car deleted"
+        />
+      </>
     );
   }
 }
